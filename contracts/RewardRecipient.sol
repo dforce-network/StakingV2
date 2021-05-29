81755751f79c92de584981e8273f1244f73bc86b
Huimin HE <joyioushe@gmail.com>
2021-05-29 22:00:28 +0800

    :recycle: Separate contracts.

---
+++ contracts/RewardRecipient.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract RewardRecipient is Ownable {
+  address public rewardDistributor;
+
+  function setRewardRate(uint256 rewardRate) external virtual;
+
+  modifier onlyRewardDistributor() {
+    require(
+      _msgSender() == rewardDistributor,
+      "Caller is not reward distribution"
+    );
+    _;
+  }
+
+  function setRewardDistributor(address _rewardDistributor) external onlyOwner {
+    rewardDistributor = _rewardDistributor;
+  }
+}
