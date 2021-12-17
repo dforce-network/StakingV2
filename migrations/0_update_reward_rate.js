6647bf916690aca9ecd09be7437ca63bc6c78ae5
Snow Ji <snow@mac.com>
2021-12-17 17:05:20 +0800

    update rewards (BSC liquidity)

--- migrations/0_update_reward_rate.js
+++ migrations/0_update_reward_rate.js
@@ -22,23 +22,25 @@
         const rewardDistributorAddress = "0x959715da68DC2D1329F4bb34e13Da03FE10c374b";
 
         let allLPPairs = [
-            // "0xBc0Aa02e6363709D84388fC9aAbedE84f2Af1Eff",   // xTSLA/USX
-            // "0xabBc34F80257B4fcb58ab6eafF9B3b70406c8C57",   // xAAPL/USX
+            "0xBc0Aa02e6363709D84388fC9aAbedE84f2Af1Eff",   // xTSLA/USX
             "0x2A41dd2c004AA2Ee42c527c1c2318F41845da2e9",   // xAMZN/USX
+            "0xabBc34F80257B4fcb58ab6eafF9B3b70406c8C57",   // xAAPL/USX
             "0x8Ee1932CA5618324E19545A0b8d3026B876e4188",   // xCOIN/USX
             "0x917ee7617aE1d03A836D2E4D5bb9EbE6C86ce125",   // BUSD/USX - kyber
             "0x5659929116fBa2F1A021228ec390CAfF7a2DE507",   // DF/USX - kyber
             "0x782F8a1375c88fD30c5c49b07a63dd97591F0ff3",   // EUX/USX - kyber
+            "0x8d61b71958dD9Df6eAA670c0476CcE7e25e98707",   // BUSD/USX - dodo
         ];
 
         let allNewRates = [
-            // '23148148148148149', 
-                        //    '23148148148148149', 
-                           '23148148148148149', 
-                           '23148148148148149', 
-                           '34722222222222223', 
-                           '81018518518518519', 
-                           '17361111111111112']
+            '17361111111111112', 
+            '17361111111111112', 
+            '17361111111111112', 
+            '17361111111111112', 
+            '5787037037037038', 
+            '57870370370370371', 
+            '11574074074074075', 
+            '57870370370370371']
 
         if (typeof remix == "object") {
             metadata = JSON.parse(
