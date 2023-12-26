940c9f4630c8de947da6840891e71b22c2d54826
l@dforce.network <liujun.leo.lll@gmail.com>
2023-12-26 20:31:51 +0800

    add contract EscrowiETHStakingPool

---
+++ contracts/dForceLending/EscrowiETHStakingPool.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./EscrowDForceLending.sol";
+
+contract EscrowiETHStakingPool is EscrowDForceLending {
+  constructor(
+    address _lp,
+    address _rewardToken,
+    uint256 _startTime,
+    uint256 _freezingTime,
+    address payable _escrowAccount
+  )
+    public
+    EscrowDForceLending(
+      _lp,
+      _rewardToken,
+      _startTime,
+      _freezingTime,
+      _escrowAccount
+    )
+  {}
+
+  function escrowUnderlyingTransfer() external onlyOwner {
+    require(FREEZING_TIME < block.timestamp, "Freezing time has not expired");
+    iETH(address(uni_lp)).redeem(
+      address(this),
+      uni_lp.balanceOf(address(this))
+    );
+    ESCROW_ACCOUNT.transfer(address(this).balance);
+  }
+
+  function mintAndStake() external payable freeze updateReward(msg.sender) {
+    uint256 _iTokenBalance = uni_lp.balanceOf(address(this));
+
+    iETH(address(uni_lp)).mint{ value: msg.value }(address(this));
+
+    address payable _sender = msg.sender;
+    uint256 _amount = (uni_lp.balanceOf(address(this))).sub(_iTokenBalance);
+    _totalSupply = _totalSupply.add(_amount);
+    _balances[_sender] = _balances[_sender].add(_amount);
+
+    emit Withdrawn(_sender, _amount);
+  }
+
+  function redeemAndWithdraw(uint256 _amount)
+    external
+    freeze
+    updateReward(msg.sender)
+  {
+    address payable _sender = msg.sender;
+    _totalSupply = _totalSupply.sub(_amount);
+    _balances[_sender] = _balances[_sender].sub(_amount);
+
+    iETH(address(uni_lp)).redeem(address(this), _amount);
+
+    _sender.transfer(address(this).balance);
+
+    emit Withdrawn(_sender, _amount);
+  }
+
+  function redeemUnderlyingAndWithdraw(uint256 _underlyingAmount)
+    external
+    freeze
+    updateReward(msg.sender)
+  {
+    uint256 _iTokenBalance = uni_lp.balanceOf(address(this));
+
+    iETH(address(uni_lp)).redeemUnderlying(address(this), _underlyingAmount);
+
+    address payable _sender = msg.sender;
+    uint256 _amount = (uni_lp.balanceOf(address(this))).sub(_iTokenBalance);
+    _totalSupply = _totalSupply.sub(_amount);
+    _balances[_sender] = _balances[_sender].sub(_amount);
+
+    _sender.transfer(_underlyingAmount);
+
+    emit Withdrawn(_sender, _amount);
+  }
+}
